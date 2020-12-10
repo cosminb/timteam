c8d0fc8e837f3005915f7f1e92c54d2d016d3ef7
danielastelia <daniela.stelia@gmail.com>
2020-12-10 18:33:21 +0200

    change building icons

--- src/Components/Buildboard.js
+++ src/Components/Buildboard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Html } from '@react-three/drei';
 import { DeviceIcon } from '../Icons/DeviceIcon';
+import { BuildingIcon } from '../Icons/BuildingIcon';
 
 export const Buildboard = ({ index }) => {
   return (
     <Html scaleFactor={300} center={true}>
       <div className="salutare">
-        <DeviceIcon />
+        <BuildingIcon />
         SITE {index}
       </div>
     </Html>
